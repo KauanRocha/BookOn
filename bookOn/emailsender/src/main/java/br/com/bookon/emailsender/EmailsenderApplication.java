655bc937162e4eff9513791be90e576ccd1e684c
Kauan Rodrigues Oliveira Rocha <kauan.rocha@yuzer.com.br>
2023-10-26 21:18:39 -0300

    Adding emailsender

---
+++ bookOn/emailsender/src/main/java/br/com/bookon/emailsender/EmailsenderApplication.java
@@ -0,0 +1,14 @@
+package br.com.bookon.emailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailsenderApplication.class, args);
+	}
+	
+
+}
